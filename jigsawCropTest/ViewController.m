188175d64028e163f01389bc9df55098602d330c
kushal bindra <kushal297@live.in>
2012-12-26 12:07:17 +0530

    code changes (had some testnig going on)

--- jigsawCropTest/ViewController.m
+++ jigsawCropTest/ViewController.m
@@ -19,63 +19,63 @@
 @implementation ViewController
 
 
-
-- (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count
-{
-    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
-    
-    // First get the image into your data buffer
-    CGImageRef imageRef = [image CGImage];
-    NSUInteger width = CGImageGetWidth(imageRef);
-    NSUInteger height = CGImageGetHeight(imageRef);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
-    NSUInteger bytesPerPixel = 4;
-    NSUInteger bytesPerRow = bytesPerPixel * width;
-    NSUInteger bitsPerComponent = 8;
-    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
-                                                 bitsPerComponent, bytesPerRow, colorSpace,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-    CGColorSpaceRelease(colorSpace);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
-    CGContextRelease(context);
-    
-    // Now your rawData contains the image data in the RGBA8888 pixel format.
-    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
-    for (int ii = 0 ; ii < count ; ++ii)
-    {
-        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
-        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
-        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
-        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
-        
-        
-        byteIndex += 4;
-        
-        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
-        
-        NSLog(@"%@",acolor);
-        
-        [result addObject:acolor];
-    }
-    CGContextRef ctx = CGBitmapContextCreate(rawData,
-                                CGImageGetWidth( imageRef ),
-                                CGImageGetHeight( imageRef ),
-                                8,
-                                CGImageGetBytesPerRow( imageRef ),
-                                CGImageGetColorSpace( imageRef ),
-                                kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
-
-      imageRef = CGBitmapContextCreateImage(ctx);
-       UIImage* rawImage = [UIImage imageWithCGImage:imageRef];
-    
-     previewIV.image = rawImage;
-    
-    free(rawData);
-    
-    return result;
-}
+//
+//- (NSArray*)getRGBAsFromImage:(UIImage*)image atX:(int)xx andY:(int)yy count:(int)count
+//{
+//    NSMutableArray *result = [NSMutableArray arrayWithCapacity:count];
+//    
+//    // First get the image into your data buffer
+//    CGImageRef imageRef = [image CGImage];
+//    NSUInteger width = CGImageGetWidth(imageRef);
+//    NSUInteger height = CGImageGetHeight(imageRef);
+//    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//    unsigned char *rawData = (unsigned char*) calloc(height * width * 4, sizeof(unsigned char));
+//    NSUInteger bytesPerPixel = 4;
+//    NSUInteger bytesPerRow = bytesPerPixel * width;
+//    NSUInteger bitsPerComponent = 8;
+//    CGContextRef context = CGBitmapContextCreate(rawData, width, height,
+//                                                 bitsPerComponent, bytesPerRow, colorSpace,
+//                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+//    CGColorSpaceRelease(colorSpace);
+//    
+//    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+//    CGContextRelease(context);
+//    
+//    // Now your rawData contains the image data in the RGBA8888 pixel format.
+//    int byteIndex = (bytesPerRow * yy) + xx * bytesPerPixel;
+//    for (int ii = 0 ; ii < count ; ++ii)
+//    {
+//        CGFloat red   = (rawData[byteIndex]     * 1.0) / 255.0;
+//        CGFloat green = (rawData[byteIndex + 1] * 1.0) / 255.0;
+//        CGFloat blue  = (rawData[byteIndex + 2] * 1.0) / 255.0;
+//        CGFloat alpha = (rawData[byteIndex + 3] * 1.0) / 255.0;
+//        
+//        
+//        byteIndex += 4;
+//        
+//        UIColor *acolor = [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+//        
+//        NSLog(@"%@",acolor);
+//        
+//        [result addObject:acolor];
+//    }
+//    CGContextRef ctx = CGBitmapContextCreate(rawData,
+//                                CGImageGetWidth( imageRef ),
+//                                CGImageGetHeight( imageRef ),
+//                                8,
+//                                CGImageGetBytesPerRow( imageRef ),
+//                                CGImageGetColorSpace( imageRef ),
+//                                kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+//
+//      imageRef = CGBitmapContextCreateImage(ctx);
+//       UIImage* rawImage = [UIImage imageWithCGImage:imageRef];
+//    
+//     previewIV.image = rawImage;
+//    
+//    free(rawData);
+//    
+//    return result;
+//}
 
 - (IBAction) testImage
 {
@@ -170,7 +170,7 @@
         }
     }
     
-    NSArray *obj = [self getRGBAsFromImage:saver atX:0 andY:0 count:saver.size.width * saver.size.height];
+    //NSArray *obj = [self getRGBAsFromImage:saver atX:0 andY:0 count:saver.size.width * saver.size.height];
 
 }
 
@@ -556,6 +556,7 @@
     }];
 }
 */
+
 - (IBAction) maskingTestAction
 {
     [self preparePuzzleImages];
